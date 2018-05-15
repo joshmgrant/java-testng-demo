10f711fe1f95c84ce6e2a0791924d1d015c88630
joshgrant <joshua.m.grant@gmail.com>
2018-05-15 07:27:31 -0700

    wip

--- src/test/java/com/demo/WorkflowTest.java
+++ src/test/java/com/demo/WorkflowTest.java
@@ -37,17 +37,17 @@ public class WorkflowTest {
             driver.findElement(By.id("sign-in")).click();
 
             Thread.sleep(2000);
+            driver.findElement(By.cssSelector("[data-test='submit']")).click();
+
+
+
+            Thread.sleep(2000);
 
             driver.findElement(By.xpath("//*[@id='session_email']")).sendKeys("jgrant@saucelabs.com");
             driver.findElement(By.xpath("//*[@id='session_password']")).sendKeys("sauce");
             driver.findElement(By.cssSelector("[data-test='submit']")).click();
 
             Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='sign-out']")).isDisplayed());
-
-            driver.findElement(By.cssSelector("[data-test='addresses']")).click();
-            Thread.sleep(2000);
-
-            Assert.assertTrue(driver.findElement(By.cssSelector("[data-test='create']")).isDisplayed());
 
         } catch (Exception e){
            System.out.println(e.toString());
