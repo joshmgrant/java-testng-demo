45bf36860fc6fcdf5021982d3b81bd115efbc1fd
joshgrant <joshua.m.grant@gmail.com>
2019-03-28 22:33:54 -0400

    w3c sample

--- src/test/java/com/demo/Tests/TestBase.java
+++ src/test/java/com/demo/Tests/TestBase.java
@@ -1,7 +1,9 @@
 package com.demo.Tests;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
@@ -19,20 +21,24 @@ public abstract class TestBase {
 
     @BeforeMethod
     public void setUp(Method method) throws MalformedURLException {
+        FirefoxOptions options = new FirefoxOptions();
 
-        DesiredCapabilities caps = DesiredCapabilities.chrome();
-        caps.setCapability("platform", "Windows 10");
-        caps.setCapability("version", "65.0");
-        caps.setCapability("seleniumVersion", "3.1.0");
-        caps.setCapability("name", method.getName());
-        caps.setCapability("build", "AddressBook-example");
+        String username = System.getenv("SAUCE_USERNAME");
+        String accesskey = System.getenv("SAUCE_ACCESS_KEY");
 
-        String sauce_username = System.getenv("SAUCE_USERNAME");
-        String sauce_access_key = System.getenv("SAUCE_ACCESS_KEY");
+        options.setCapability("browserVersion", "65.0");
+        options.setCapability("platformName", "Windows 10");
 
-        URL url = new URL("https://" + sauce_username + ":" + sauce_access_key + "@ondemand.saucelabs.com/wd/hub/");
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("seleniumVersion", "3.141.59");
+        sauceOptions.setCapability("name", method.getName());
+        sauceOptions.setCapability("build", "W3C");
 
-        driver = new RemoteWebDriver(url, caps);
+        options.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://" + username + ":" + accesskey + "@ondemand.saucelabs.com/wd/hub/");
+
+        driver = new RemoteWebDriver(url, options);
 
         driver.get("http://a.testaddressbook.com");
 
